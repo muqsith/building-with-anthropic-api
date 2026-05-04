e4b1c0c8623cd947f36d6b2de1440cc3bc2e9be0
mui <mui@one.com>
2026-05-04 09:13:45 +0400

    math tutor

---
+++ math-tutor.py
@@ -0,0 +1,46 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from anthropic import Anthropic
+
+client = Anthropic()
+model = "claude-opus-4-7"
+
+def add_user_message(messages, text):
+    messages.append({"role": "user", "content": text})
+
+def add_assistant_message(messages, text):
+    messages.append({"role": "assistant", "content": text})
+
+def chat(messages, system=None):
+    params = {
+        "model": model,
+        "max_tokens": 1000,
+        "messages": messages,
+    }
+    
+    if system:
+        params["system"] = system
+    
+    message = client.messages.create(**params)
+    return message.content[0].text
+
+messages = []
+
+system_prompt = """
+You are a patient math tutor.
+Do not directly answer a student's questions.
+Guide them to a solution step by step.
+"""
+
+while True:
+    user_input = input("> ")
+    if user_input.strip().lower() in ("quit", "exit"):
+        break
+
+    print("---")
+    add_user_message(messages, user_input)
+    response = chat(messages, system=system_prompt)
+    add_assistant_message(messages, response)
+    print(response)
+    print("---")
